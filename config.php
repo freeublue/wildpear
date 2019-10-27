0dc59c6bc6b3872093effef2ee7ba0b0ea935cc4
Susan Hern <freeublue@gmail.com>
2019-10-27 07:37:31 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "t";
-$dbname = "";
-$dbuser = "c";
-$dbpass = "d";
+$dbhost = "us-cdbr-iron-east-05.cleardb.net";
+$dbname = "heroku_32bf49c2ccea664";
+$dbuser = "be36fc0f49a07c";
+$dbpass = "fa5b12fd";
   
          
          $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
