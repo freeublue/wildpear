9e66d017157a31eee05ebbeaee93ed7076a3a31d
Susan Hern <freeublue@gmail.com>
2019-10-27 07:38:13 +0200

    Add files via upload

--- adminsql/processeditaccommodation.php
+++ adminsql/processeditaccommodation.php
@@ -1,12 +1,12 @@
 <?
 require "../bootstraptop.php";
 require "../config.php";
-$title = mysqli_real_esacpe_string($conn, $_POST['title']);
-$price = mysqli_real_esacpe_string($conn, $_POST['price']);
-$sup = mysqli_real_esacpe_string($conn, $_POST['sup']);
-$numberpers = mysqli_real_esacpe_string($conn, $_POST['numberpers']);
-$descp = mysqli_real_esacpe_string($conn, $_POST['descp']);
-$facilities = mysqli_real_esacpe_string($conn, $_POST['facilities']);
+$title = mysqli_real_escape_string($conn, $_POST['title']);
+$price = mysqli_real_escape_string($conn, $_POST['price']);
+$sup = mysqli_real_escape_string($conn, $_POST['sup']);
+$numberpers = mysqli_real_escape_string($conn, $_POST['numberpers']);
+$descp = mysqli_real_escape_string($conn, $_POST['descp']);
+$facilities = mysqli_real_escape_string($conn, $_POST['facilities']);
 $id = $_POST[id];
 $sq = mysqli_query($conn, "UPDATE accommodation1 SET title = '$title', 
 descp = '$descp', price = '$price',
