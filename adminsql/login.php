1921c5f2ec6177a4b079c263347dedf15309b7a8
Susan Hern <freeublue@gmail.com>
2019-11-01 20:28:44 +0200

    Update login.php

--- adminsql/login.php
+++ adminsql/login.php
@@ -1,5 +1,5 @@
 <?php
-include "bootstraptop.php";
+
 echo "<form action='inlog.php' method='post'>Username.. 
 <input type='text' name='login' /><br />Password.. 
 <input type='password' name='password' /><br />Log In.......... <input type='submit' class='submit' name='submit' value='login' /></form><br />";
