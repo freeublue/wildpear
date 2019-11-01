458bb4614e296ad39e1d94b23dc7014cf9175804
Susan Hern <freeublue@gmail.com>
2019-11-01 20:47:50 +0200

    Add files via upload

--- adminsql/inlog.php
+++ adminsql/inlog.php
@@ -6,7 +6,7 @@ $pass = trim($_POST[password]);
 $q = mysqli_query($conn, "SELECT * FROM amz WHERE am1 = '$name'");
 while($row = mysqli_fetch_array($q ) )  { 
 echo "<b>Hi </b> $row[am1]<br />";
-
+$_SESSION['customer'] = $name;
 if(md5($pass) === $row[am2]) { 
 $_SESSION['customer'] = $name;
 echo "<b><a href='index.php'>Click to proceed</a><br />"; } }
