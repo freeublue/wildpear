3b2185484761ba2fff994387cba5cdca6f8475e5
Susan Hern <freeublue@gmail.com>
2019-11-02 10:11:07 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "us-cdbr-iron-east-05.cleardb.net";
-$dbname = "heroku_32bf49c2ccea664";
-$dbuser = "be36fc0f49a07c";
-$dbpass = "fa5b12fd";
+$dbhost = "host";
+$dbname = "dbname";
+$dbuser = "user";
+$dbpass = "password";
   
          
          $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
